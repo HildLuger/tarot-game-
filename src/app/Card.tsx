e61145510aa9ffcadf6efeb19b669f3ebfedeb23
HildLuger <88519194+HildLuger@users.noreply.github.com>
2024-01-11 04:16:31 -0300

    cart.tsx preload images

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,6 +1,6 @@
-// src/components/Card.tsx
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Head from 'next/head';
 import { useDispatch, useSelector } from 'react-redux';
 import { flipCard, resetGame } from './slices/arcanaSlice';
 import { RootState } from './store';
@@ -10,6 +10,9 @@ export const ArcanaCard = () => {
   const dispatch = useDispatch();
   const [isFlipping, setIsFlipping] = useState(false);
   const { currentCard, allCardsRevealed } = useSelector((state: RootState) => state.arcana);
+
+  // Generate an array with the paths of all 79 cards
+  const cardImages = Array.from({ length: 79 }, (_, i) => `/Arcana${i}.jpg`);
 
   const handleFlip = () => {
     if (!allCardsRevealed && !isFlipping) {
@@ -29,6 +32,11 @@ export const ArcanaCard = () => {
 
   return (
     <div className={styles.arcanaContainer}>
+      <Head>
+        {cardImages.map((src, index) => (
+          <link key={index} rel="preload" href={src} as="image" />
+        ))}
+      </Head>
       <div onClick={handleFlip} className={`${styles.card} ${isFlipping ? styles.flipped : ''}`}>
         <Image 
           src={`/${currentCard}`} 
@@ -38,12 +46,10 @@ export const ArcanaCard = () => {
           className={styles.cardImage}
           loading="eager" 
           priority={currentCard === "Arcana0.jpg"} // Add priority if the current card is Arcana0.jpg
-        
         />
-      </div >
+      </div>
       {allCardsRevealed && (
-        <div className= " text-sm rounded-lg text-center">
-        
+        <div className="text-sm rounded-lg text-center">
           <button 
             onClick={() => dispatch(resetGame())} 
             className="bg-purple-700 dark:bg-blue-900 text-white p-2 rounded hover:bg-purple-500 dark:hover:bg-blue-600 transition-colors absolute bottom-20 right-10"
